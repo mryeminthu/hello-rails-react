1be32ebf38722b7082a68301dc126a601de8d13d
mryeminthu <dhammaarchive@hotmail.com>
2024-01-22 16:13:43 +0630

    Set root page

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root 'home#index'
+  get '/api/random_greeting', to: 'greetings#get_random_greeting'
 end
